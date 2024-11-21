a2378cfd738bc78b7afefbf922e6282cd2758c72
jaquelynhyde <52418434+jaquelynhyde@users.noreply.github.com>
2024-11-20 22:05:54 -0600

    Finalish version
    Lots of changes, big refactoring.
    
    Change to measurement mode with 'm'. Click on contours with the left mouse button. Measurements (in pixels) will be automatically generated. Toggle measurement mode on and off to take new measurements. To use the test image instead of the webcam, change the use_loaded_image variable to True

--- EdgeMeasurementUtility.py
+++ EdgeMeasurementUtility.py
@@ -11,170 +11,264 @@
 
 # Luhana Zorrilla
 
-from pathlib import Path
-
 import numpy as np
 import cv2 as cv
+import json
 
-# Given:    Two 2D points
-# Return:   The euclidean distance between them
-def calculate_distance(pt1, pt2):
-    return np.sqrt((pt2[0] - pt1[0]) ** 2 + (pt2[1] - pt1[1]) ** 2)
-
-# Given:    An image
-# Return:   An edge-detected image
-def detect_edges(image, min_val, max_val):
-    print('Detecting edges...')
-    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    
-    edges = cv.Canny(gray, min_val, max_val)
-    
-    return edges
-
-# Given:    An image with a calibration target
-# Return:   The ratio of millimeters to pixels in the image
-def calibrate_mm_to_pixel_ratio():
-    return 1
-
-# Given:    A set of two contours and a desired direction (horizontal/vertical)
-# Return:   The minimum and maximum distance between them in that direction
-#            Ideally, this would also provide a visualization of these measurements
-#           Or return a matrix representing them that can be used to visualize them
-def measure_contour_distances():
-    return 1
-
-def mouse_callback(event, x, y, flags, param):
-    if event == cv.EVENT_LBUTTONDOWN:
-        return (x,y)       
-
-# to do:
-    # move junk into functions
-    # make it so the output image is more of the screen.. we dont need input
-    # 
-def user_interface():
-    print('Initializing user interface')
-
-    # ui parameters
-    wait_time = 25
-    height = 768
-    width = 1360 
-    panel_height = height 
-    panel_width = width 
-    window_name = 'Edge Measurement Utility'
-    
-    min_val = 127
-    max_val = 200
-    
-    # environment states
-    live_contours = True
-    measurement_mode = False
-    manual_measurement_mode = False
-    
-    # input keys
-    live_contour_key = 'd'
-    measurement_mode_key = 'm'
-    manual_measurement_key = 'n'
-    export_contours_key = 'c'
-
-    ui_image = np.zeros((height, width, 3), np.uint8)    
-    print('ui panel created')
-
-    cv.namedWindow(window_name)
-
-    cap = cv.VideoCapture(0)
-    
-    if not cap.isOpened():
-        print('Unable to open camera')
-        exit()
-    
-    # we need to determine some properties about the camera pre-loop
-    ret, frame = cap.read()
-    
-    last_frame = frame
-    
-    f_height, f_width, channels = frame.shape
-    
-    scaling_factor_x = f_height // height
-    scaling_factor_y = f_width // width 
-    
-    mm_pixel_ratio = calibrate_mm_to_pixel_ratio()
-
-    while 1:
-        print('looping...')
+class EdgeMeasurementUtility:
+    def __init__(self):
+        self.edges = None
+        self.edges_color = None
+        self.contours = None
+        self.hierarchy = None
+        self.selected_contours = []
+        self.hor_contours = None
+        self.ver_contours = None
+        self.closest_hor_contour = None
+        self.closest_ver_contour = None
+        self.furthest_hor_contour = None
+        self.furthest_ver_contour = None
+        self.image = None
+        self.last_image = None
+        self.json_data = None
+        self.mm_pixel_ratio = 1
+        self.use_loaded_image = False
+        self.loaded_image = cv.imread(cv.samples.findFile("Test_Image.jpg"))
         
-        if not measurement_mode:
-            ret, frame = cap.read()
-        else:
-            frame = last_frame
+    @staticmethod
+    def horizontal_distance(point1, point2):
+        return abs(point1[0] - point2[0])
         
-        last_frame = frame
+    @staticmethod
+    def vertical_distance(point1, point2):
+        return abs(point1[1] - point2[1])
+        
+    # Given:    An image with a calibration target
+    # Return:   The ratio of millimeters to pixels in the image
+    @staticmethod
+    def calibrate_mm_to_pixel_ratio():
+        return 1
+    
+    # Given:    An image
+    # Return:   An edge-detected image    
+    @staticmethod
+    def detect_edges(image, min_val, max_val):
+        print('Detecting edges...')
+        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        
+        edges = cv.Canny(gray, min_val, max_val)
+        
+        return edges
+    
+    def select_contour(self, x, y): 
+        print('select_contour at x: ' + str(x) + ' / y: ' + str(y))   
+        
+        largest_idx = -1
+        largest_area = -1
 
-        if not ret:
-            print('Unable to read frame')
-            break
+        for i, contour in enumerate(self.contours):
+            if cv.pointPolygonTest(contour, (x, y), False) >= 0 and len(self.selected_contours) < 2:
+                area = cv.contourArea(contour)
+                if area > largest_area:
+                    largest_idx = i
+                    largest_area = area
+                
+        if largest_idx != -1 and len(self.selected_contours) < 2:
+            self.selected_contours.append(largest_idx)
+            cv.drawContours(self.image, self.contours[largest_idx], -1, (255, 255, 0), thickness = 3)
+            cv.imshow(self.window_name, self.image)
+            print('found contour! length is now: ' + str(len(self.selected_contours)))
+            if len(self.selected_contours) == 2:
+                contour1 = self.contours[self.selected_contours[0]]
+                contour2 = self.contours[self.selected_contours[1]]                   
+                    
+                min_hor_dist = float('inf')
+                max_hor_dist = float(0)
+                min_ver_dist = float('inf')
+                max_ver_dist = float(0)
+                    
+                closest_hor_points = None
+                furthest_hor_points = None
+                    
+                closest_ver_points = None
+                furthest_ver_points = None
+                    
+                for point1 in contour1:
+                    for point2 in contour2:
+                        if point1[0][1] == point2[0][1]:
+                            dist = self.horizontal_distance(point1[0], point2[0])
+                            if dist < min_hor_dist:
+                                min_hor_dist = dist
+                                closest_hor_points = (point1[0], point2[0])
+                            if dist > max_hor_dist:
+                                max_hor_dist = dist
+                                furthest_hor_points = (point1[0], point2[0])
+                        if point1[0][0] == point2[0][0]:
+                            dist = self.vertical_distance(point1[0], point2[0])
+                            if dist < min_ver_dist:
+                                min_ver_dist = dist
+                                closest_ver_points = (point1[0], point2[0])
+                            if dist > max_ver_dist:
+                                max_ver_dist = dist
+                                furthest_ver_points = (point1[0], point2[0])
+                                    
+                if closest_hor_points:
+                    self.closest_hor_contour = np.array([
+                        closest_hor_points[0],
+                        closest_hor_points[1],
+                        (closest_hor_points[1][0], closest_hor_points[1][1] - 1),
+                        (closest_hor_points[0][0], closest_hor_points[0][1] - 1)],
+                        dtype = np.int32)
+                    
+                if closest_ver_points:
+                    self.closest_ver_contour = np.array([
+                        closest_ver_points[0],
+                        closest_ver_points[1],
+                        (closest_ver_points[1][0], closest_ver_points[1][1] - 1),
+                        (closest_ver_points[0][0], closest_ver_points[0][1] - 1)],
+                        dtype = np.int32)
+                        
+                if furthest_hor_points:
+                    self.furthest_hor_contour = np.array([
+                        furthest_hor_points[0],
+                        furthest_hor_points[1],
+                        (furthest_hor_points[1][0], furthest_hor_points[1][1] - 1),
+                        (furthest_hor_points[0][0], furthest_hor_points[0][1] - 1)],
+                        dtype = np.int32)
+                        
+                if furthest_ver_points:
+                    self.furthest_ver_contour = np.array([
+                        furthest_ver_points[0],
+                        furthest_ver_points[1],
+                        (furthest_ver_points[1][0], furthest_ver_points[1][1] - 1),
+                        (furthest_ver_points[0][0], furthest_ver_points[0][1] - 1)],
+                        dtype = np.int32)
+                        
+                if self.closest_hor_contour is not None:
+                    cv.drawContours(self.image, [self.closest_hor_contour], -1, (255, 0, 0), thickness = cv.FILLED)
+                    x, y, w, h = cv.boundingRect(self.closest_hor_contour)
+                    cv.putText(self.image, 'closest horizontal distance : ' + str(min_hor_dist) + ' pixels', (0, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv.LINE_AA)
+                        
+                if self.closest_ver_contour is not None:
+                    cv.drawContours(self.image, [self.closest_ver_contour], -1, (255, 0, 0), thickness = cv.FILLED)
+                    x, y, w, h = cv.boundingRect(self.closest_ver_contour)
+                    cv.putText(self.image, 'closest vertical distance : ' + str(min_ver_dist) + ' pixels', (0, 60), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv.LINE_AA)
+                        
+                if self.furthest_hor_contour is not None:
+                    cv.drawContours(self.image, [self.furthest_hor_contour], -1, (0, 0, 255), thickness = cv.FILLED)
+                    x, y, w, h = cv.boundingRect(self.furthest_hor_contour)
+                    cv.putText(self.image, 'furthest horizontal distance : ' + str(max_hor_dist) + ' pixels', (0, 90), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv.LINE_AA)
+                        
+                if self.furthest_ver_contour is not None:
+                    cv.drawContours(self.image, [self.furthest_ver_contour], -1, (0, 0, 255), thickness = cv.FILLED)
+                    x, y, w, h = cv.boundingRect(self.furthest_ver_contour)
+                    cv.putText(self.image, 'furthest vertical distance : ' + str(max_ver_dist) + ' pixels', (0, 120), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv.LINE_AA)
+                        
+                distances = { 
+                    "min_horizontal_distance": float(min_hor_dist * self.mm_pixel_ratio),
+                    "max_horizontal_distance": float(max_hor_dist * self.mm_pixel_ratio),
+                    "min_vertical_distance": float(min_ver_dist * self.mm_pixel_ratio),
+                    "max_vertical_distance": float(max_ver_dist * self.mm_pixel_ratio)
+                    }
+                    
+                self.json_data = json.dumps(distances, indent=4)
+                    
+                print(json.dumps(distances, indent=4))
+                    
+                cv.imshow(self.window_name, self.image)
 
-        edges = detect_edges(frame, min_val, max_val)
+                        
+    def mouse_callback_wrapper(self):
+        def mouse_callback(event, x, y, flags, param):
+            if event == cv.EVENT_LBUTTONDOWN:
+                self.select_contour(x, y)
+        return mouse_callback
+    
+    def user_interface(self):
+        # ui parameters
+        wait_time = 25
+        
+        self.window_name = 'Edge Measurement Utility'
+        
+        min_val = 127
+        max_val = 200
+        
+        measurement_mode = False # toggle between live feed and measuring on an image
+        
+        measurement_mode_key = 'm'
+        
+        cv.namedWindow(self.window_name)
 
-        print('Finding contours...')
-        contours, hierarchy = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-        print('Converting output to RGB...')
-        edges_color = cv.cvtColor(edges, cv.COLOR_GRAY2BGR)        
-
-        if live_contours:
-            print('Drawing contours...')
-            cv.drawContours(edges_color, contours, -1, (0,255,0),2)
+        cap = cv.VideoCapture(0)
+        
+        if not cap.isOpened():
+            print('Unable to open camera')
+            exit()
+        
+        # we need to determine some properties about the camera pre-loop
+        ret, frame = cap.read()
+        
+        self.image = frame.copy()
+        
+        if self.use_loaded_image == True:
+            self.image = self.loaded_image.copy()
             
- #       print('Resizing input...')
- #       resized_input = cv.resize(frame, (panel_width, panel_height))
- #       resized_output = cv.resize(edges_color, (panel_width, panel_height))
- #       
- #       print('Modifying ui image...')
- #       ui_image[0:panel_height, 0:panel_width] = resized_input
- #       ui_image[0:panel_height, panel_width:width] = resized_output
-
-        print('Resizing output to screen')
-        resized_output = cv.resize(edges_color, (panel_width, panel_height))
+        f_height, f_width, channels = frame.shape
         
-        ui_image[0:panel_height, 0:panel_width] = resized_output
+        cv.resizeWindow(self.window_name, f_height, f_width)
 
-        print('Showing ui image...')
-        cv.imshow(window_name, ui_image)
+        while True:            
+            if not measurement_mode:
+                self.selected_contours.clear()
+                self.closest_hor_contour = None
+                self.closest_ver_contour = None
+                self.furthest_hor_contour = None
+                self.furthest_ver_contour = None
+                ret, frame = cap.read()
+                if not ret:
+                    print('Unable to read frame')
+                    break
+                
+                self.image = frame.copy()
+                
+            if self.use_loaded_image == True:
+                self.image = self.loaded_image.copy()
+            
+            self.edges = self.detect_edges(self.image, min_val, max_val)
+            
+            if measurement_mode:
+                self.contours, self.hierarchy = cv.findContours(self.edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+                self.edges_color = cv.cvtColor(self.edges, cv.COLOR_GRAY2BGR) 
+                cv.drawContours(self.edges_color, self.contours, -1, (0,255,0), thickness = 1)
+            else:
+                self.edges_color = cv.cvtColor(self.edges, cv.COLOR_GRAY2BGR) 
+    
+            self.image = self.edges_color
+
+            cv.imshow(self.window_name, self.image)
+            
+            if measurement_mode:
+                self.last_image = self.image
+                cv.setMouseCallback(self.window_name, self.mouse_callback_wrapper())
+                input_key = cv.waitKey(0)
+            else:
+                cv.setMouseCallback(self.window_name, lambda *args : None)
+                input_key = cv.waitKey(wait_time)             
+
+            if input_key & 0xFF == 27:
+                break
+            elif cv.getWindowProperty(self.window_name, cv.WND_PROP_VISIBLE) < 1:
+                break
+            elif input_key & 0xFF == ord(measurement_mode_key):
+                measurement_mode = not measurement_mode
+
+        print('Destroying windows and releasing camera')
+        cap.release()
+        cv.destroyAllWindows()
         
-        print('Awaiting input...')
-        # set up mouse callback if in measurement mode
-        if measurement_mode:
-            cv.setMouseCallback(window_name, mouse_callback)
-            print('a')
-            input_key = cv.waitKey(0)
-            print('b')            
-        else:
-            input_key = cv.waitKey(wait_time) 
-            print('c')
-        
-
-        if input_key & 0xFF == 27:
-            break
-        elif cv.getWindowProperty(window_name, cv.WND_PROP_VISIBLE) < 1:
-            break
-        elif input_key & 0xFF == ord(live_contour_key):
-            live_contours = not live_contours
-        elif input_key & 0xFF == ord(measurement_mode_key):
-            measurement_mode = not measurement_mode
-        elif input_key & 0xFF == ord(manual_measurement_key):
-            manual_measurement_mode = not manual_measurement_mode
-
-
-    print('Destroying windows and releasing camera')
-    cap.release()
-    cv.destroyAllWindows()
-    
-    print('Exit complete.')
-    return 0
-    
-    
-def main():
-    user_interface()
-
+        print('Exit complete.')
+        return 0
+                        
 if __name__ == "__main__":
-    main()
+    tool = EdgeMeasurementUtility()
+    tool.user_interface()
